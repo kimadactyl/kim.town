d5d9a94655e035fb14eb839d7fbd4556a9b4fd50
Dr Kim Foale <kim@gfsc.studio>
2023-09-03 21:15:48 +0100

    Fix baseurl

--- quartz.config.ts
+++ quartz.config.ts
@@ -9,7 +9,7 @@ const config: QuartzConfig = {
     analytics: {
       provider: "plausible",
     },
-    baseUrl: "quartz.jzhao.xyz",
+    baseUrl: "kim.town",
     ignorePatterns: ["private", "templates", ".obsidian"],
     defaultDateType: "created",
     theme: {
